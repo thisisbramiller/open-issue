f3adac1ae5f1a9e3f63925d15261a9360db2e8f3
bramiller1989 <76793954+bramiller1989@users.noreply.github.com>
2021-01-06 17:10:51 -0600

    feat(action) add create issue action

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const token = core.getInput("token");
+    const title = core.getInput("title");
+    const body = core.getInput("body");
+    const assignees = core.getInput("assignees");
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const octokit = github.getOctokit(token);
 
-    core.setOutput('time', new Date().toTimeString());
+    const response = await octokit.issues.create({
+      // owner: github.context.repo.owner,
+      // repo: github.context.repo.repo,
+      ...github.context.repo,
+      title,
+      body,
+      assignees: assignees ? assignees.split("\n") : undefined
+    });
+
+    core.setOutput("issue", JSON.stringify(response.data));
   } catch (error) {
     core.setFailed(error.message);
   }
