ec7a83704342cd04876491082be21f6908f9df04
pipo02mix <fernando@giantswarm.io>
2023-02-07 12:08:56 +0100

    Check if title issue matches

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ const github = require("@actions/github");
 
 async function run() {
   try {
+    var issues = [];
     const token = core.getInput("token");
+    const check_exists = core.getInput("check_exists");
     const title = core.getInput("title");
     const body = core.getInput("body");
     const assignees = core.getInput("assignees");
 
     const octokit = github.getOctokit(token);
+
+    if (check_exists) {
+      let q = title + ":title";
+      issues = octokit.rest.search.issuesAndPullRequests({
+        q,
+      });
+    }
+    
+    core.setOutput("issue exist response: ", JSON.stringify(issues));
 
     const response = await octokit.issues.create({
       // owner: github.context.repo.owner,
